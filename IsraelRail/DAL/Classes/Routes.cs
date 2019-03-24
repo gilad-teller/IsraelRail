413d5c998c97395d41fda37f816c7f13ff68d5c7
giladteller <giladteller@gmail.com>
2019-03-25 00:14:06 +0200

    DAL, object, http client

---
+++ IsraelRail/DAL/Classes/Routes.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Classes
+{
+
+    public class Rootobject
+    {
+        public int MessageType { get; set; }
+        public object Message { get; set; }
+        public Data Data { get; set; }
+    }
+
+    public class Data
+    {
+        public object Error { get; set; }
+        public Details Details { get; set; }
+        public int StartIndex { get; set; }
+        public Route[] Routes { get; set; }
+        public object BeforeRoutes { get; set; }
+        public object[] Rates { get; set; }
+        public object[] Delays { get; set; }
+        public object[] TrainPositions { get; set; }
+        public Omasim[] Omasim { get; set; }
+    }
+
+    public class Details
+    {
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public string Date { get; set; }
+        public string SugKav { get; set; }
+        public string Hour { get; set; }
+    }
+
+    public class Route
+    {
+        public Train[] Train { get; set; }
+        public bool IsExchange { get; set; }
+        public string EstTime { get; set; }
+    }
+
+    public class Train
+    {
+        public string Trainno { get; set; }
+        public string OrignStation { get; set; }
+        public string DestinationStation { get; set; }
+        public string ArrivalTime { get; set; }
+        public string DepartureTime { get; set; }
+        public Stopstation[] StopStations { get; set; }
+        public string LineNumber { get; set; }
+        public string Route { get; set; }
+        public bool Midnight { get; set; }
+        public bool Handicap { get; set; }
+        public bool DirectTrain { get; set; }
+        public object TrainParvariBenironi { get; set; }
+        public bool ReservedSeat { get; set; }
+        public string Platform { get; set; }
+        public string DestPlatform { get; set; }
+        public bool IsFullTrain { get; set; }
+    }
+
+    public class Stopstation
+    {
+        public string StationId { get; set; }
+        public string ArrivalTime { get; set; }
+        public string DepartureTime { get; set; }
+        public string Platform { get; set; }
+    }
+
+    public class Omasim
+    {
+        public int TrainNumber { get; set; }
+        public Station[] Stations { get; set; }
+    }
+
+    public class Station
+    {
+        public int StationNumber { get; set; }
+        public float OmesPercent { get; set; }
+        public string Shmurim { get; set; }
+        public string Time { get; set; }
+        public int Platform { get; set; }
+    }
+
+}
---
+++ IsraelRail/DAL/Client.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Client
+    {
+        private static HttpClient httpClient;
+        private static object _lock = new object();
+
+        public static HttpClient GetClient
+        {
+            get
+            {
+                if (httpClient == null)
+                {
+                    lock (_lock)
+                    {
+                        if (httpClient == null)
+                        {
+                            httpClient = new HttpClient();
+                        }
+                    }
+                }
+                return httpClient;
+            }
+        }
+
+        public static async Task<T> Get<T,K>(K request)
+        {
+            GetClient.BaseAddress = new Uri("https://www.rail.co.il/apiinfo/api/");
+            using (HttpResponseMessage response = await GetClient.GetAsync("Plan/GetRoutes?OId=700&TId=4600&Date=20190325&Hour=2400&isGoing=true"))
+            {
+                response.EnsureSuccessStatusCode();
+                string content = await response.Content.ReadAsStringAsync();
+                return default(T);
+            }
+        }
+    }
+}
