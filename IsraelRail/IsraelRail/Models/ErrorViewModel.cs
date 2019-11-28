0b31e2763dc810bf07a91a8c5ff33cf51a38bf46
giladteller <giladteller@gmail.com>
2019-11-28 13:13:37 +0200

    core project

---
+++ IsraelRail/IsraelRail/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IsraelRail.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
